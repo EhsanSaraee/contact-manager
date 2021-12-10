df9107d0a409e1f36acf279b19b7679dc5b15f86
ehsan <ehsansaraee6570@yahoo.com>
2021-12-10 11:23:12 +0330

    main components layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+   return (
+      <main className="App">
+         <h1>Contact Manager</h1>
+         <section>Add Contact</section>
+         <section>Contact List</section>
+      </main>
+   );
+};
+
+export default App;
