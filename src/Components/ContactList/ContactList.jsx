6864b50aaf1df208f2f158baa4e43f52e6bc0085
ehsan <ehsansaraee6570@yahoo.com>
2021-12-10 21:37:43 +0330

    add styles to ContactList component

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -4,16 +4,22 @@ import './ContactList.css';
 
 const ContactList = ({ contacts, onDelete }) => {
    return (
-      <section className="contactList">
-         <div>
-            <h2>Contacts</h2>
-            <Link to="/add-contact">
-               <button>Add</button>
-            </Link>
+      <section className="listWrapper">
+         <div className="contactList">
+            <div className="listHeader">
+               <h2>Contacts</h2>
+               <Link to="/add-contact">
+                  <button>Add</button>
+               </Link>
+            </div>
+            {contacts.map((contact) => (
+               <Contact
+                  key={contact.id}
+                  contact={contact}
+                  onDelete={onDelete}
+               />
+            ))}
          </div>
-         {contacts.map((contact) => (
-            <Contact key={contact.id} contact={contact} onDelete={onDelete} />
-         ))}
       </section>
    );
 };
